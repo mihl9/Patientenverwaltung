42ef6306ed3a168555b190e4ff50f686d45f7668
sandro-pedrett <sandro.pedrett@hotmail.com>
2015-05-06 14:01:54 +0200

    create project

---
+++ Patientenverwaltung/src/ch/gbssg/core/pac/ICommand.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @modtime 13:28:46
+ * @author pedrett
+ */
+package ch.gbssg.core.pac;
+
+
+/**
+ * Interface for commands between messages.
+ * 
+ * @author pedrett
+ * @version 1.0
+ */
+public interface ICommand {
+	
+}
