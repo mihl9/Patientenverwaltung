d1117990b5e778ee812ab6801703a09561f266a3
sandro-pedrett <sandro.pedrett@hotmail.com>
2015-05-16 13:29:09 +0200

    patient table with details

--- Patientenverwaltung/src/ch/gbssg/app/ila/doctor/DoctorController.java
+++ Patientenverwaltung/src/ch/gbssg/app/ila/doctor/DoctorController.java
@@ -1,6 +1,9 @@
 package ch.gbssg.app.ila.doctor;
 
+import java.time.LocalDate;
+
 import javafx.scene.layout.Pane;
+import ch.gbssg.app.model.Patient;
 import ch.gbssg.app.util.command.CmdShowUi;
 import ch.gbssg.core.pac.AgentCommand;
 import ch.gbssg.core.pac.AgentController;
@@ -11,8 +14,20 @@ public class DoctorController extends AgentController {
 	@Override
 	public boolean setupAgent() {
 		model = new DoctorModel();
-		view = new DoctorView(this);
+		view = new DoctorView(this, model);
 		
+		Patient p1 = new Patient();
+		p1.setFirstname("Sandro");
+		p1.setLastname("Pedrett");
+		p1.setBirthday(LocalDate.now());
+		
+		Patient p2 = new Patient();
+		p2.setFirstname("Aline");
+		p2.setLastname("Schweizer");
+		p2.setPlace("Hemberg");
+		
+		model.getPatientsData().add(p1);
+		model.getPatientsData().add(p2);
 		return true;
 	}
 
--- Patientenverwaltung/src/ch/gbssg/app/model/Patient.java
+++ Patientenverwaltung/src/ch/gbssg/app/model/Patient.java
@@ -1,8 +1,13 @@
 package ch.gbssg.app.model;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import ch.gbssg.core.AbsModel;
 
 /**
@@ -14,19 +19,29 @@ public class Patient extends AbsModel {
 
 	private int id;
 	private int genderCode;
-	private String firstname;
-	private String lastname;
-	private Date birthday;
-	private String address;
-	private String plz;
-	private String place;
-	private String insuranceNumber;
+	private StringProperty firstname;
+	private StringProperty lastname;
+	private ObjectProperty<LocalDate> birthday;
+	private StringProperty address;
+	private StringProperty plz;
+	private StringProperty ahv;
+	private StringProperty place;
+	private StringProperty insuranceNumber;
 	
+	public Patient() {
+		firstname = new SimpleStringProperty();
+		lastname = new SimpleStringProperty();
+		address = new SimpleStringProperty();
+		plz = new SimpleStringProperty();
+		ahv = new SimpleStringProperty();
+		place = new SimpleStringProperty();
+		insuranceNumber = new SimpleStringProperty();
+		birthday = new SimpleObjectProperty<LocalDate>(LocalDate.now());
+	}
 	
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
@@ -44,74 +59,104 @@ public class Patient extends AbsModel {
 
 
 	public String getFirstname() {
-		return firstname;
+		return firstname.get();
 	}
 
 
 	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+		this.firstname.set(firstname);
 	}
 
+	public StringProperty getFirstnameProperty() {
+		return firstname;
+	}
 
 	public String getLastname() {
-		return lastname;
+		return lastname.get();
 	}
 
 
 	public void setLastname(String lastname) {
-		this.lastname = lastname;
+		this.lastname.set(lastname);
+	}
+
+	public StringProperty getLastnameProperty() {
+		return lastname;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday.get();
 	}
 
 
-	public Date getBirthday() {
-		return birthday;
-	}
-
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
+	public void setBirthday(LocalDate birthday) {
+		this.birthday.set(birthday);
 	}
 
 
 	public String getAddress() {
-		return address;
+		return address.get();
 	}
 
 
 	public void setAddress(String address) {
-		this.address = address;
+		this.address.set(address);
 	}
 
+	public StringProperty getAddressProperty() {
+		return address;
+	}
+	
+	public String getAhv() {
+		return ahv.get();
+	}
+	
+	public void setAhv(String ahv) {
+		this.ahv.set(ahv);
+	}
+	
+	public StringProperty getAhvProperty() {
+		return ahv;
+	}
 
 	public String getPlz() {
-		return plz;
+		return plz.get();
 	}
 
 
 	public void setPlz(String plz) {
-		this.plz = plz;
+		this.plz.set(plz);
 	}
 
+	public StringProperty getPlzProberty() {
+		return plz;
+	}
 
 	public String getPlace() {
-		return place;
+		return place.get();
 	}
 
 
 	public void setPlace(String place) {
-		this.place = place;
+		this.place.set(place);
 	}
 
+	public StringProperty getPlaceProperty() {
+		return place;
+	}
 
 	public String getInsuranceNumber() {
-		return insuranceNumber;
+		return insuranceNumber.get();
 	}
 
 
 	public void setInsuranceNumber(String insuranceNumber) {
-		this.insuranceNumber = insuranceNumber;
+		this.insuranceNumber.set(insuranceNumber);
 	}
 
+	public StringProperty getInsuranceNumberProperty() {
+		return insuranceNumber;
+	}
 
 	@Override
 	public boolean isValid(List<String> errors) {
