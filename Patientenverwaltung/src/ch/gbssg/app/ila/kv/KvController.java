23fccf04332fbb3b6fd0e1ac5166e387a9cf830a
Michael Huber <michael-13@live.de>
2015-05-27 15:12:36 +0200

    Added controlsFX

--- Patientenverwaltung/src/ch/gbssg/app/ila/kv/KvController.java
+++ Patientenverwaltung/src/ch/gbssg/app/ila/kv/KvController.java
@@ -1,10 +1,15 @@
 package ch.gbssg.app.ila.kv;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import javax.naming.spi.DirStateFactory.Result;
+
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.layout.Pane;
 import ch.gbssg.app.model.Code;
 import ch.gbssg.app.model.Faktura;
+import ch.gbssg.app.model.MedicalHistory;
 import ch.gbssg.app.util.command.CmdDoExport;
 import ch.gbssg.app.util.command.CmdFilterEntity;
 import ch.gbssg.app.util.command.CmdShowUi;
@@ -65,9 +70,32 @@ public class KvController extends AgentController {
 	public void generateInvoice(Faktura model){
 		CmdDoExport cmd = new CmdDoExport("InvoiceTemplate.docx");
 		cmd.addDataModel("faktura", model);
+		
 		//cmd.setExportType(ExportType.PDF);
 		sendAgentMessage(new AgentCommand(cmd));
 		
+	}
+	
+	public void changeState(Faktura model){
+		if(model!=null){
+			MedicalHistory medHistory = new MedicalHistory();
+			medHistory.setId(model.getId());
+			
+			CmdFilterEntity<MedicalHistory> getData = new CmdFilterEntity<MedicalHistory>(MedicalHistory.class, medHistory);
+			sendAgentMessage(new AgentCommand(getData));
+			
+			medHistory = getData.getEntities().get(0);
+			
+			ChoiceDialog<Code> dialog = new ChoiceDialog<>(getAssignedCode(medHistory.getBillState()), this.model.getCodesData());
+			dialog.setTitle("Neuer Status");
+			dialog.setHeaderText("Bitte wählen Sie den neuen Status aus.");
+			dialog.setContentText("Status:");
+			
+			Optional<Code> result = dialog.showAndWait();
+			
+			result.ifPresent(choice -> System.out.println(choice.getId()));
+			
+		}
 	}
 	public void FilterTable(Code state, LocalDate dateFrom, LocalDate dateTo){
 		model.getFakturenFilteredData().clear();
